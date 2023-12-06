af17821c108287009030aa0bff4fb2a40d90477e
taron-dev <taron-dev@taron.sk>
2023-12-06 09:13:20 +0100

    Create tables only if not already exist

--- src/main/resources/db/changelog/changesets/V20231119184645__initTables.sql
+++ src/main/resources/db/changelog/changesets/V20231119184645__initTables.sql
@@ -2,7 +2,7 @@
 -- changeset taron-dev:V20231119184645__initTables.sql
 
 -- Create Category table
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
   id BIGINT PRIMARY KEY,
   name VARCHAR(255),
   label VARCHAR(255),
@@ -13,7 +13,7 @@ ALTER TABLE category MODIFY id BIGINT AUTO_INCREMENT;
 ALTER TABLE category AUTO_INCREMENT=100;
 
 -- Create Intention table
-CREATE TABLE intention (
+CREATE TABLE IF NOT EXISTS intention (
   id BIGINT PRIMARY KEY,
   text VARCHAR(255),
   category_id BIGINT,
