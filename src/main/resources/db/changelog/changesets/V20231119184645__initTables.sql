d338b78bfa59801dca3672d4aba5a851176a672c
Ondrej Tarasovič <taron-dev@taron.sk>
2023-12-14 19:19:36 +0100

    1 (#5)
    * Add first dockerfile
    
    * Create tables only if not already exist
    
    * Comment out foreign key from migration

--- src/main/resources/db/changelog/changesets/V20231119184645__initTables.sql
+++ src/main/resources/db/changelog/changesets/V20231119184645__initTables.sql
@@ -2,7 +2,7 @@
 -- changeset taron-dev:V20231119184645__initTables.sql
 
 -- Create Category table
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
   id BIGINT PRIMARY KEY,
   name VARCHAR(255),
   label VARCHAR(255),
@@ -13,12 +13,12 @@ ALTER TABLE category MODIFY id BIGINT AUTO_INCREMENT;
 ALTER TABLE category AUTO_INCREMENT=100;
 
 -- Create Intention table
-CREATE TABLE intention (
+CREATE TABLE IF NOT EXISTS intention (
   id BIGINT PRIMARY KEY,
   text VARCHAR(255),
   category_id BIGINT,
-  intention_id BINARY(16) NOT NULL UNIQUE,
-  FOREIGN KEY (category_id) REFERENCES category(id)
+  intention_id BINARY(16) NOT NULL UNIQUE
+  --FOREIGN KEY (category_id) REFERENCES category(id) - not supported by planetscale free db
 );
 
 ALTER TABLE intention MODIFY id BIGINT AUTO_INCREMENT;
